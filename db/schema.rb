e07ddec20bffc7c90925c4dc3573808309bce151
WANGXUANDING <wangxuanding1@gmail.com>
2018-05-28 03:24:02 +0200

    fin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426081603) do
+ActiveRecord::Schema.define(version: 20180426082956) do
 
   create_table "admin_jobs", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180426081603) do
     t.integer  "wage_upper_bound"
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
+    t.boolean  "is_hidden"
   end
 
   create_table "users", force: :cascade do |t|
